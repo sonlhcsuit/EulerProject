fc62b24bb3c13b50d3c151cef2bba5f1fe33abc9
sonlhcsuit <sonlh.cs.uit@gmail.com>
2021-05-17 16:30:52 +0700

    update & refactor source name

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def sum_of_2(degree = 1000):
-    return sum(map(int,str(2**degree)))
-a = sum_of_2()
-print(a)
+a = "some_string"
+print(id(a))
+print(id("some" + "_" + "string")) # Notice that both the ids are same.
+array = [1, 8, 15]
+# A typical generator expression
+gen = (x for x in array if array.count(x) > 0)
+array = [2, 8, 22]
+print(list(gen))
